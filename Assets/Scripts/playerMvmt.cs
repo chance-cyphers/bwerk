0aeb259dbab2d1567495a28250ed4b169046ad58
Heather <heather.myomass@gmail.com>
2018-09-13 16:09:38 -0700

    a message

---
+++ Assets/Scripts/playerMvmt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMvmt : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var z = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+
+        transform.Translate(0, 0, z);
+    }
+}
