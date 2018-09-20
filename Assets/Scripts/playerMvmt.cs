e4efeed565336f24e0b1767b83665c9d3ccc3276
Heather <heather.myomass@gmail.com>
2018-09-20 16:28:29 -0700

    Walk and cube

--- Assets/Scripts/playerMvmt.cs
+++ Assets/Scripts/playerMvmt.cs
@@ -7,15 +7,44 @@ public class playerMvmt : MonoBehaviour {
     private Rigidbody rb;
     public float speed;
 
+    bool bLeftReq, bRightReq;
+
+    Vector3 vDirectModifier;
+   
+
     void Start () {
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update () {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(0.0f, 0.0f, moveHorizontal);
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+            bLeftReq = true;
+        }
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            bRightReq = true;
+        }
+            
+    }
 
-        rb.AddForce(movement * 50);
+
+    void FixedUpdate()
+    {
+        vDirectModifier.z = 0;
+
+        if (bLeftReq)
+        {
+            bLeftReq = false;
+            vDirectModifier.z -= speed;
+        }
+        if (bRightReq)
+        {
+            bRightReq = false;
+            vDirectModifier.z += speed;
+        }
+
+        rb.velocity = vDirectModifier;
     }
 }
