844de0c4e6a088c320f1ca0631b294b3473fdcdc
chance <chance.a.cyphers@gmail.com>
2018-09-21 12:48:29 -0400

    WIP: fixing character mvmt and velocity

--- Assets/Scripts/playerMvmt.cs
+++ Assets/Scripts/playerMvmt.cs
@@ -1,84 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class playerMvmt : MonoBehaviour {
 
-    private Rigidbody rb;
-    private Vector3 jump;
-    private Vector3 horizontalMoveForce;
-    private Vector3 otherMoveForce;
-    public float speed;
+    CharacterController characterController;
+    public float speed = 18.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
 
-    bool bLeftReq, bRightReq;
-
-    Vector3 vDirectModifier;
-    private bool isGrounded;
-    public float jumpForce = 2.0f;
+    Vector3 moveDirection = Vector3.zero;
 
     void Start () {
-        rb = GetComponent<Rigidbody>();
-        jump = new Vector3(0.0f, 2.0f, 0.0f);
-        horizontalMoveForce = new Vector3(0.0f, 0.0f, 2.0f);
-        otherMoveForce = new Vector3(2.0f, 0.0f, 0.0f);
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update () {
+        moveDirection = new Vector3(Input.GetAxis("Vertical"), 0.0f, Input.GetAxis("Horizontal"));
+        moveDirection *= speed;
 
-        // WALK
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetButton("Jump"))
         {
-            rb.AddForce(-speed * horizontalMoveForce, ForceMode.Force);
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            rb.AddForce(speed * horizontalMoveForce, ForceMode.Force);
+            moveDirection.y = jumpSpeed;
         }
 
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            rb.AddForce(-speed * otherMoveForce, ForceMode.Force);
-        }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            rb.AddForce(speed * otherMoveForce, ForceMode.Force);
-        }
-
-
-        // JUMP
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
-            isGrounded = false;
-        }
-
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
     }
-
-
-    void FixedUpdate()
-    {
-        /*vDirectModifier.z = 0;
-
-        // WALK
-        if (bLeftReq)
-        {
-            bLeftReq = false;
-            vDirectModifier.z -= speed;
-        }
-        if (bRightReq)
-        {
-            bRightReq = false;
-            vDirectModifier.z += speed;
-        }
-
-        rb.velocity = vDirectModifier;*/
-    }
-
-    void OnCollisionStay()
-    {
-        isGrounded = true;
-    }
-
 
 }
