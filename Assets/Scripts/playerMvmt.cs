dd548b21aed166f3b086fa499461b1cc3d78bf70
Heather <heather.myomass@gmail.com>
2018-09-20 17:26:19 -0700

    I guess walk and jump

--- Assets/Scripts/playerMvmt.cs
+++ Assets/Scripts/playerMvmt.cs
@@ -5,35 +5,53 @@ using UnityEngine;
 public class playerMvmt : MonoBehaviour {
 
     private Rigidbody rb;
+    private Vector3 jump;
+    private Vector3 moveForce;
     public float speed;
 
     bool bLeftReq, bRightReq;
 
     Vector3 vDirectModifier;
-   
+    private bool isGrounded;
+    public float jumpForce = 2.0f;
 
     void Start () {
         rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        moveForce = new Vector3(0.0f, 0.0f, 2.0f);
     }
 
     // Update is called once per frame
     void Update () {
+
+        // WALK
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-            {
+        {
+            rb.AddForce(-speed * moveForce, ForceMode.Force);
             bLeftReq = true;
         }
             else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
+            rb.AddForce(speed * moveForce, ForceMode.Force);
             bRightReq = true;
         }
-            
+
+
+        // JUMP
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
     }
 
 
     void FixedUpdate()
     {
-        vDirectModifier.z = 0;
+        /*vDirectModifier.z = 0;
 
+        // WALK
         if (bLeftReq)
         {
             bLeftReq = false;
@@ -45,6 +63,13 @@ public class playerMvmt : MonoBehaviour {
             vDirectModifier.z += speed;
         }
 
-        rb.velocity = vDirectModifier;
+        rb.velocity = vDirectModifier;*/
     }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+
 }
